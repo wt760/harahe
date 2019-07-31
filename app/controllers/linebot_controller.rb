0a1325e00633fc07892a5e6a14c68fe1c30709db
shumaikunkun <s1711520@gmail.com>
2019-08-01 01:23:11 +0900

    選択肢を出せるように

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -7,7 +7,7 @@ class LinebotController < ApplicationController
   def client
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]   
     }
   end
 
@@ -15,26 +15,45 @@ class LinebotController < ApplicationController
     body = request.body.read
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      head :bad_request
-    end
+    head :bad_request unless client.validate_signature(body, signature)
 
-    events = client.parse_events_from(body)
 
-    events.each { |event|
+    #arr=["オハヨー","おやすみ","うるせー","こんにちは","こんにちま","こんちは"]
+
+    client.parse_events_from(body).each do |event|
       case event
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
           message = {
-            type: 'text',
-            text: event.message['text']
+            type: "template",
+            altText: "this is a confirm template",
+            template: {
+              type: "confirm",
+              #type: 'text',
+              #text: arr[rand(arr.size)]
+              #text: event.message['text']
+              #text: event["message"]["text"]
+              text: "ラーメン食べたい？",
+              actions: [
+                {
+                  type: "message",
+                  label: "はい",
+                  text: "ラーメン食べたい"
+                },
+                {
+                  type: "message",
+                  label: "いいえ",
+                  text: "ラーメン食べたくない"
+                }
+              ]
+            }
           }
           client.reply_message(event['replyToken'], message)
         end
       end
-    }
-
+    end
     head :ok
   end
+
 end
