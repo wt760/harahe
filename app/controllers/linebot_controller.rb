612c4cc48112cd126c382f1cfe734a132140775b
shumaikunkun <s1711520@gmail.com>
2019-08-01 11:03:58 +0900

    質問文と選択肢を一般化してループで回す

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -7,7 +7,7 @@ class LinebotController < ApplicationController
   def client
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]   
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
   end
 
@@ -17,43 +17,46 @@ class LinebotController < ApplicationController
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     head :bad_request unless client.validate_signature(body, signature)
 
+    arr_que=["","場所は？","時間は？","ラーメン食べたい？","中華食べたい？","洋食食べたい？","和食食べたい？"]
+    arr_yes=["検索","現在地を使う","現在時刻","ラーメン食べたい！","中華食べたい！","洋食食べたい！","和食食べたい！"]
+    arr_noo=["検索","場所を指定する","時間を指定する","ラーメン食べたくない…","中華食べたくない…","洋食食べたくない…","和食食べたくない…"]
 
-    #arr=["オハヨー","おやすみ","うるせー","こんにちは","こんにちま","こんちは"]
+    (arr_que.size-1).times do |i|
+      client.parse_events_from(body).each do |event|
+        if event.class == Line::Bot::Event::Message
+          if event.type == Line::Bot::Event::MessageType::Text
+            if event["message"]["text"]==arr_yes[i]||event["message"]["text"]==arr_noo[i]
 
-    client.parse_events_from(body).each do |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          message = {
-            type: "template",
-            altText: "this is a confirm template",
-            template: {
-              type: "confirm",
-              #type: 'text',
-              #text: arr[rand(arr.size)]
-              #text: event.message['text']
-              #text: event["message"]["text"]
-              text: "ラーメン食べたい？",
-              actions: [
-                {
-                  type: "message",
-                  label: "はい",
-                  text: "ラーメン食べたい"
-                },
-                {
-                  type: "message",
-                  label: "いいえ",
-                  text: "ラーメン食べたくない"
+              message = {
+                type: "template",
+                altText: "this is a confirm template",
+                template: {
+                  type: "confirm",
+                  #type: 'text',a
+                  #text: event.message['text']
+                  #text: event["message"]["text"]
+                  text: arr_que[i+1],
+                  actions: [
+                    {
+                      type: "message",
+                      label: arr_yes[i+1],
+                      text: arr_yes[i+1]
+                    },
+                    {
+                      type: "message",
+                      label: arr_noo[i+1],
+                      text: arr_noo[i+1]
+                    }
+                  ]
                 }
-              ]
-            }
-          }
-          client.reply_message(event['replyToken'], message)
+              }
+              client.reply_message(event['replyToken'], message)
+            end
+          end
         end
       end
+      head :ok
     end
-    head :ok
   end
 
 end
